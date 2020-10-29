fa7a855dca8b3ff152684a85f9333cf173191578
Art Longbottom <art.longbottom.jr@gmail.com>
2020-10-29 16:33:57 -0700

    generate module types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
-export { default as ColorPicker } from "./components/ColorPicker/ColorPicker";
-export * from "./types";
+import ColorPicker from "./components/ColorPicker/ColorPicker";
+import {
+  RGBA,
+  HSVA,
+  HSLA,
+  HEX,
+  RGBA_FORMAT,
+  HSLA_FORMAT,
+  HSVA_FORMAT,
+  HEX_FORMAT,
+  HEX8_FORMAT,
+  COLOR_FORMATS,
+  COLORS,
+  COLOR,
+} from "./types";
+
+export {
+  ColorPicker,
+  RGBA,
+  HSVA,
+  HSLA,
+  HEX,
+  RGBA_FORMAT,
+  HSLA_FORMAT,
+  HSVA_FORMAT,
+  HEX_FORMAT,
+  HEX8_FORMAT,
+  COLOR_FORMATS,
+  COLORS,
+  COLOR,
+};
